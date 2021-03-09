6b7bf5c7ce0f44b48cbc762594a2af05383f342e
Xuân Hưng <xuanhungptithcm@gmail.com>
2021-03-09 19:49:14 +0700

    first commit

---
+++ array.js
@@ -0,0 +1,27 @@
+//! 1. push
+//! 2. pop
+//! 3. unshift
+//! 4. shift
+//! 5. forEach
+//! 6. some
+//! 7. every
+//! 8. map
+//! 9. reduce
+//! 10. filter
+//! 11. find
+//! 12. findIndex
+//! 13. includes
+//! 14. indexOf
+//! 15. fill
+//! 16. concat
+//! 17. reverse
+//! 18. sort
+//! 19. flat
+//! 20. join
+//! 21.splice
+//! 22.slice
+
+
+// - Tham khảo thêm tại:https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
+
+// - Giải thích cách mà hàm includes so sánh https://developer.mozilla.org/en-US/docs/Web/JavaScript/Equality_comparisons_and_sameness#same-value-zero_equality
